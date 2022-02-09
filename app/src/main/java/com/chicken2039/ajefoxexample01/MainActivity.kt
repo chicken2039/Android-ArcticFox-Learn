7acb7e6f85d9057b679c77fd5bb930d30609d04f
chicken2039 <zoq2039@gmail.com>
2022-02-10 02:15:03 +0900

    #life cycle test

--- app/src/main/java/com/chicken2039/ajefoxexample01/MainActivity.kt
+++ app/src/main/java/com/chicken2039/ajefoxexample01/MainActivity.kt
@@ -2,10 +2,63 @@ package com.chicken2039.ajefoxexample01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import com.chicken2039.ajefoxexample01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val TAG = "StateChange"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.i(TAG,"onCreate")
+        convertCurrency()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG,"onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        Log.i(TAG,"onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG,"onRestoreInstanceState")
+    }
+
+
+    fun convertCurrency(){
+        binding.mainText.text= "hello aje"
+
     }
 }
